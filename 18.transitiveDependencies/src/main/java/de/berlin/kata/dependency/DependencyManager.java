7220b168a1d6747367be76a8716c1d3a33d88720
guiquan <weng.guiquan@gmail.com>
2015-04-22 10:04:33 +0200

    small refactoring.

--- 18.transitiveDependencies/src/main/java/de/berlin/kata/dependency/DependencyManager.java
+++ 18.transitiveDependencies/src/main/java/de/berlin/kata/dependency/DependencyManager.java
@@ -1,35 +1,37 @@
 package de.berlin.kata.dependency;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class DependencyManager {
     private Map<String, List<String>> dependencyMap = new HashMap<>();
 
-    public void addDependencies(String word, String... dependencies) {
-        this.dependencyMap.put(word, Arrays.asList(dependencies));
+    public void addDependencies(String component, String... dependencies) {
+        this.dependencyMap.put(component, Arrays.asList(dependencies));
     }
 
-    public List<String> getDependencies(String start) {
-        List<String> dependencies = new ArrayList<>(this.dependencyMap.get(start));
+    public List<String> getDependencies(String component) {
+        List<String> dependencies = new ArrayList<>(this.dependencyMap.get(component));
 
         for (int i = 0; i < dependencies.size(); i++) {
             String dependency = dependencies.get(i);
             List<String> transitiveDependencies = this.dependencyMap.get(dependency);
 
             if (transitiveDependencies != null) {
-                dependencies.addAll(transitiveDependencies);
-                dependencies = getUniqueDependencies(dependencies);
+                addUniqueDependencies(dependencies, transitiveDependencies);
             }
         }
 
         // remove self.
-        dependencies.remove(start);
+        dependencies.remove(component);
         Collections.sort(dependencies);
         return dependencies;
     }
 
-    private List<String> getUniqueDependencies(List<String> dependencies) {
-        return dependencies.stream().distinct().collect(Collectors.toList());
+    private void addUniqueDependencies(List<String> dependencies, List<String> transitiveDependencies) {
+        transitiveDependencies.forEach(newDependency -> {
+            if (!dependencies.contains(newDependency)) {
+                dependencies.add(newDependency);
+            }
+        });
     }
 }
